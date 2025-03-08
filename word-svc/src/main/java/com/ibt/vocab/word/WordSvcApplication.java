c7bb16002b5f35d1f9d4c53d6de4923159e1c32f
Mahesh Patil <maheshpatil@Mac.lan>
2025-03-08 22:30:19 +0000

    Add logging and import from json

--- word-svc/src/main/java/com/ibt/vocab/word/WordSvcApplication.java
+++ word-svc/src/main/java/com/ibt/vocab/word/WordSvcApplication.java
@@ -6,34 +6,63 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import com.ibt.vocab.word.model.Word;
 import com.ibt.vocab.word.service.WordService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.type.TypeReference;
+import org.springframework.core.io.ClassPathResource;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @SpringBootApplication
 public class WordSvcApplication implements CommandLineRunner {
 
-	@Autowired
-	private WordService wordService;
+    @Autowired
+    private WordService wordService;
 
-	public static void main(String[] args) {
-		SpringApplication.run(WordSvcApplication.class, args);
-	}
+    @Override
+    public void run(String... args) throws Exception {
+        // Print initial count
+        log.info("Initial word count: " + wordService.count());
 
-	@Override
-	public void run(String... args) throws Exception {
-		Word word = new Word();
-		word.setWord("hello");
-		word.setMeaning("greeting");
-		word.setExamples(Arrays.asList("hello world", "hello there"));
-		wordService.addWord(word);
+        // Read JSON file
+        ObjectMapper mapper = new ObjectMapper();
+        ClassPathResource resource = new ClassPathResource("import-words.json");
+        Map<String, List<Word>> wordsMap = mapper.readValue(
+            resource.getInputStream(),
+            new TypeReference<Map<String, List<Word>>>() {}
+        );
 
-		Word word1 = new Word();
-		word1.setWord("hello1");
-		word1.setMeaning("greeting");
-		word1.setExamples(Arrays.asList("hello world", "hello there"));
+        // Add words from JSON
+        List<Word> words = wordsMap.get("words");
+        for (Word word : words) {
+            wordService.addWord(word);
+        }
 
-		wordService.addWord(word);
-		wordService.addWord(word1);
-		System.out.println(wordService.count());
-	}
+        // Add your existing test words
+        Word word = new Word();
+        word.setWord("hello");
+        word.setMeaning("greeting");
+        word.setExamples(Arrays.asList("hello world", "hello there"));
+        wordService.addWord(word);
 
+        Word word1 = new Word();
+        word1.setWord("hello1");
+        word1.setMeaning("greeting");
+        word1.setExamples(Arrays.asList("hello world", "hello there"));
+        wordService.addWord(word1);
+
+        // Print final count
+
+        log.info("All words:");
+        log.debug(mapper.writeValueAsString(wordService.getAllWords()));
+        log.info("Final word count: " + wordService.count());
+
+
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(WordSvcApplication.class, args);
+    }
 }
